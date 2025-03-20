a70b4af82c2ee345a9e189baa103266b23ec8ba0
ded-moon <70951560+ded-moon@users.noreply.github.com>
2025-03-20 20:16:55 +0200

    Added custom 404 page

---
+++ 404.js
@@ -0,0 +1,32 @@
+var messages = [
+    'Not found',
+    'Not here',
+    'huh?',
+    'This page doesn\'t exist',
+    'Maybe try again?',
+    'You\'re lost',
+    'You\'re lost',
+    'Nope',
+    'Nope.',
+    'nope',
+    'nope.',
+    'Call tech support',
+    'Call tech support maybe?',
+    'What are you doing here?',
+    '\"Oh. Hi. So... How are you holding up?',
+    'Search deeper maybe',
+    'Search deeper maybe.',
+    'Search deeper maybe?',
+    'This page was wiped from the face of the earth',
+    'This page was wiped from the face of the earth',
+    'This page was wiped from the face of the earth.',
+    'The page you\'re trying to reach no longer exists',
+    'The page you\'re trying to reach no longer exists.',
+    'empty',
+    "Are you lost?",
+    'error 404, error mesage not found',
+]
+function newMessage() {
+        var randomNumber = Math.floor(Math.random() * (messages.length));
+        document.getElementById('messsageDisplay').innerHTML = messages[randomNumber];
+}
